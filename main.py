a81b87d27695cee3bf7ef25ed4e111ec74c97d86
Aleksey <dmitrievalexsey94@gmail.com>
2026-09-19 11:30:20 +0300

    lesson 1

---
+++ main.py
@@ -0,0 +1,14 @@
+name = "Aleksey"
+age = 32
+height = 1.69
+is_student = True
+nothing = None
+
+print(name, age, height, is_student, nothing)
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+year = int("2026")
+print(year + 1)
