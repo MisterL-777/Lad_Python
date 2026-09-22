20348309c92b5115d3d8e996374f8f6cb115696c
Aleksey <dmitrievalexsey94@gmail.com>
2026-09-22 18:45:54 +0300

    feat: homework 1

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-name = "Aleksey"
-age = 32
-height = 1.69
-is_student = True
-nothing = None
+# Программа создает таблицу "фактов о себе"
 
-print(name, age, height, is_student, nothing)
-print(type(name))
-print(type(age))
-print(type(height))
-print(type(is_student))
+name = "Aleksey"     #str
+age = 32             #int
+height = 1.69        #float
+like_number = 11     #int
+love_python = True   #bool
 
-year = int("2026")
-print(year + 1)
+# Используем f-строки для формирования
+print(f"Имя: {name}")
+print(f"Возраст: {age}")
+print(f"Рост: {height}")
+print(f"Любимое число: {like_number}")
+print(f"Люблю Python: {love_python}")
+
+# Проверка типов данных
+print(type(name))           #<class "str">
+print(type(age))            #<class "int">
+print(type(height))         #<class "float">
+print(type(like_number))    #<class "int">
+print(type(love_python))    #<class "bool">
+
+
+# python -m venv создает исключительно изолированное виртуальное окружение, в то время как uv init инициализирует полноценную структуру Python-проекта. Использовать python -m venv, если мне нужно простое, чистое виртуальное окружение без привязки к конкретному менеджеру проектов. Использовать uv init, если я начинаю разработку нового приложения или библиотеки и хочу получить современный инструмент «всё в одном» (замена pip, venv, poetry и pyenv).
